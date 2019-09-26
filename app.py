899fe8a308e4aa6fe72efb4421b3ec38cbdc977f
Mike Barkmin <mike.barkmin@uni-due.de>
2019-09-26 20:05:02 +0200

    Add data fetching

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_url_path="/static")
 
@@ -6,3 +6,8 @@ app = Flask(__name__, static_url_path="/static")
 @app.route("/")
 def root():
     return render_template("index.html")
+
+
+@app.route("/data")
+def data():
+    return jsonify(["here", "is", "some", "data"])
--- static/Hallo.jsx
+++ static/Hallo.jsx
@@ -1,1 +1,19 @@
-const Hallo = () => <div>Hallo React from Flask!</div>;
+const Hallo = () => {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('/data')
+      .then(res => res.json())
+      .then(json => setData(json));
+  }, []);
+  return (
+    <>
+      <div>Hallo React from Flask!</div>
+      <ul>
+        {data.map(d => (
+          <li key={d}>{d}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
